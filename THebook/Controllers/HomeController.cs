1209190e9ba0c454aa97d523401295d600dff22b
bhbghghbgb <hgbboyhuynhgiabao122003@yahoo.com>
2024-09-27 12:45:46 +0700

    tao prokect

---
+++ THebook/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace THebook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "HomePage")]
+        public IDictionary<string, string> Get([FromQuery] string? name) =>
+            new Dictionary<string, string>
+            {
+                { "Hello", "World" },
+                { "Me may", "Beo" },
+                { "Ten me may", name! },
+            };
+    }
+}
