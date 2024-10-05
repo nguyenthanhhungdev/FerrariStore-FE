d17df1074846dff43e21eea7006b48511b812c7b
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-10-05 10:43:16 +0700

    Update Book Schema

--- backend/THebook/Models/Book.cs
+++ backend/THebook/Models/Book.cs
@@ -10,16 +10,36 @@ namespace THebook.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; } = null!;
 
-        [BsonElement("name")]
-        [JsonPropertyName("name")]
-        public string? Name { get; set; } = null!;
+        [BsonElement("title")]
+        [JsonPropertyName("title")]
+        public string? Title { get; set; } = null!;
 
-        [BsonElement("authors")]
-        [JsonPropertyName("authors")]
-        public string? Authors { get; set; } = null!;
+        [BsonElement("description")]
+        [JsonPropertyName("description")]
+        public string? Description { get; set; } = null!;
 
-        [BsonElement("year")]
-        [JsonPropertyName("year")]
-        public int? Year { get; set; } = null!;
+        [BsonElement("cover_image")]
+        [JsonPropertyName("cover_image")]
+        public string? CoverImage { get; set; } = null!;
+
+        [BsonElement("file_path")]
+        [JsonPropertyName("file_path")]
+        public string? FilePath { get; set; } = null!;
+
+        [BsonElement("published_year")]
+        [JsonPropertyName("published_year")]
+        public int? PublishedYear { get; set; } = null!;
+
+        [BsonElement("language")]
+        [JsonPropertyName("language")]
+        public string? Language { get; set; } = null!;
+
+        [BsonElement("created_at")]
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonElement("updated_at")]
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
     }
 }
